fd218341d00ad2ec1b71534399bef45a3b6c6844
WallaceFalue <wallaceffalque@gmail.com>
2025-09-04 13:59:17 -0300

    Layout changed, more fields added

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -3,8 +3,11 @@ import EmployeeRegister from "./components/EmployeeRegister";
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-slate-600">
-      <div className="w-1/3 p-6 bg-white rounded shadow-md">
+    <div className="min-h-screen bg-gray-500 flex items-center justify-center">
+      <div className="w-full max-w-4xl p-8 bg-white rounded-xl shadow-lg">
+        <h1 className="text-3xl font-bold text-blue-700 text-center mb-8">
+          Employee Registration
+        </h1>
         <EmployeeRegister />
       </div>
     </div>
--- my-react-app/src/components/EmployeeRegister.tsx
+++ my-react-app/src/components/EmployeeRegister.tsx
@@ -1,43 +1,133 @@
+import { useState } from "react";
+
 function EmployeeRegister() {
+  const [form, setForm] = useState({
+    name: "",
+    lastName: "",
+    company: "",
+    position: "",
+    department: "",
+    email: "",
+    registration: "",
+    salary: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
+  };
+
+  const click = () => {
+    console.log(form);
+  };
+
   return (
-    <div className="flex flex-row items-start gap-x-8">
-      <div className="flex flex-col w-full max-w-xs">
-        <input
-          className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
-          type="text"
-          placeholder="Name"
-        />
+    <div className="fflex flex-row items-start gap-x-8 bg-white p-8 rounded-xl shadow-lg max-w-3xl mx-auto mt-10">
+      <label className="font-semibold text-gray-700 mb-1" htmlFor="name">
+        First Name*
+      </label>
+      <input
+        name="name"
+        className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
+        value={form.name}
+        onChange={handleChange}
+        type="text"
+        placeholder="Name"
+      />
 
-        <input
-          className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
-          type="text"
-          placeholder="Last name"
-        />
-      </div>
+      <label className="font-semibold text-gray-700 mb-1" htmlFor="lastName">
+        Last Name*
+      </label>
+      <input
+        name="lastName"
+        className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
+        value={form.lastName}
+        onChange={handleChange}
+        type="text"
+        placeholder="Last name"
+      />
 
-      <div className="flex flex-col w-full max-w-xs">
-        <input
-          className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
-          type="text"
-          placeholder="Company"
-        />
+      <label className="font-semibold text-gray-700 mb-1" htmlFor="email">
+        Enter Email*
+      </label>
+      <input
+        name="email"
+        className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
+        value={form.email}
+        onChange={handleChange}
+        type="text"
+        placeholder="E-mail"
+      />
 
-        <input
-          className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
-          type="text"
-          placeholder="Position"
-        />
+      <label
+        className="font-semibold text-gray-700 mb-1"
+        htmlFor="registration"
+      >
+        Registration*
+      </label>
+      <input
+        name="registration"
+        className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
+        value={form.registration}
+        onChange={handleChange}
+        type="text"
+        placeholder="Registration"
+      />
 
-        <input
-          className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
-          type="text"
-          placeholder="Department"
-        />
+      <label className="font-semibold text-gray-700 mb-1" htmlFor="company">
+        Company*
+      </label>
+      <input
+        name="company"
+        className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
+        value={form.company}
+        onChange={handleChange}
+        type="text"
+        placeholder="Company"
+      />
 
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 font-bold self-start">
-          Registrar
-        </button>
-      </div>
+      <label className="font-semibold text-gray-700 mb-1" htmlFor="position">
+        Position*
+      </label>
+      <input
+        name="position"
+        className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
+        value={form.position}
+        onChange={handleChange}
+        type="text"
+        placeholder="Position"
+      />
+
+      <label className="font-semibold text-gray-700 mb-1" htmlFor="department">
+        Department*
+      </label>
+      <input
+        name="department"
+        className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
+        value={form.department}
+        onChange={handleChange}
+        type="text"
+        placeholder="Department"
+      />
+
+      <label className="font-semibold text-gray-700 mb-1" htmlFor="salary">
+        Salary*
+      </label>
+      <input
+        name="salary"
+        className="border-2 border-gray-300 rounded p-2 w-full mb-4 text-gray-700 bg-white"
+        value={form.salary}
+        onChange={handleChange}
+        type="text"
+        placeholder="Salary"
+      />
+
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 font-bold self-start"
+        onClick={click}
+      >
+        Register
+      </button>
     </div>
   );
 }
